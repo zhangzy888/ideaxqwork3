2c0acfc867a9f64479858a7d9d2e3ab2231f58d8
小七 <https://github.com/52xiaoqi88/xq.git>
2018-12-24 18:31:59 +0800

    web 第一次上传

---
+++ springboot-dubbo-wob-jpo/src/main/java/com/jk/springbootdubbowobjpo/SpringbootDubboWobJpoApplication.java
@@ -0,0 +1,14 @@
+package com.jk.springbootdubbowobjpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDubboWobJpoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDubboWobJpoApplication.class, args);
+    }
+
+}
+
---
+++ springboot-dubbo-wob-jpo/src/test/java/com/jk/springbootdubbowobjpo/SpringbootDubboWobJpoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.jk.springbootdubbowobjpo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootDubboWobJpoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
